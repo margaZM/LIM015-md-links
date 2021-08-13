2cf748a87c11963b271b27edb57bb28293c3a2da
margaZM <aurazambrano227@gmail.com>
2021-08-13 16:55:22 -0500

    Agrega la funcionalidad de la funcion de stats

--- src/md-links.js
+++ src/md-links.js
@@ -1,29 +1,53 @@
 #!/usr/bin/env node
 const api = require('./api');
+const cli = require('./cli');
 require('colors');
 const program = require('commander')
     .version('0.1.0')
     .command('md-links <path>')
     .description('Valid links inside file markdown')
     .option('-v, --validate [value]', 'valid links', false)
+    .option('-s, --stats', 'stats of links')
     .action(function(path, options) {
-        api.mdLinks(path, options).then((resp) => {
-            for (const url of resp) {
-                if ( options.validate === false ) {
-                    console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text}`)
-                    console.log('------------------------------------------------------------------')
-                }else {
-                    if(url.status < 299){
-                        console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status} ${'|'.yellow}   U+08A7 ${url.ok.green}`)
+        if (options.stats === true && options.validate === true) {
+            api.mdLinks(path, options).then((resp) => {
+                let urls = resp;
+                cli.optionValidate(urls)
+                .then(val => {
+                    let stats = cli.optionStats(urls);
+                    stats.Broken = cli.optionStatsValidate(val).Broken;
+                    console.log(`Total: ${stats.Total}`);
+                    console.log(`Unique: ${stats.Unique}`);
+                    console.log(`Broken: ${stats.Broken}`);
+                })
+            })
+        } else if (options.stats === true) {
+            api.mdLinks(path, options).then((resp) => {
+                let urls = resp;
+                const stats = cli.optionStats(urls);
+                console.log(`Total: ${stats.Total}`)
+                console.log(`Unique: ${stats.Unique}`)
+            })
+        }else {
+            api.mdLinks(path, options).then((resp) => {
+                for (const url of resp) {
+                    if ( options.validate === false ) {
+                        console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text}`)
                         console.log('------------------------------------------------------------------')
+                    }else {
+                        if(url.status < 299){
+                            console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status} ${'|'.yellow}   U+08A7 ${url.ok.green}`)
+                            console.log('------------------------------------------------------------------')
 
-                    } else {
-                        console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status.red} ${'|'.yellow}  ${url.ok.red}`)
-                        console.log('------------------------------------------------------------------')
+                        } else {
+                            console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status.red} ${'|'.yellow}  ${url.ok.red}`)
+                            console.log('------------------------------------------------------------------')
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
+
     })
 
 program.parse(process.argv);
