414bc88803852ab4ca98653b3b3998bc9e16225b
margaZM <aurazambrano227@gmail.com>
2021-08-16 20:33:53 -0500

    Refactoriza output de funcion md-links

--- src/md-links.js
+++ src/md-links.js
@@ -1,54 +1,54 @@
 #!/usr/bin/env node
 const api = require('./api');
 const cli = require('./cli');
+const spinner = require('./spinner');
+const logUpdate = require('log-update');
 require('colors');
+// var emoji = require('node-emoji');
 const program = require('commander')
     .version('0.1.0')
     .command('md-links <path>')
-    .description('Valid links inside file markdown')
-    .option('-v, --validate [value]', 'valid links', false)
-    .option('-s, --stats', 'stats of links')
+    .description('Parses Markdown files and prints the links it contains, their path, status and stats')
+    .option('-v, --validate [value]', 'Validates the links in the .md file', false)
+    .option('-s, --stats', 'Statistics associated with the links')
     .action(function(path, options) {
+
+    api.mdLinks(path, options).then((resp) => {
+        // spinner.spinner();
         if (options.stats === true && options.validate === true) {
-            api.mdLinks(path, options).then((resp) => {
-                let urls = resp;
-                cli.optionValidate(urls)
-                .then(val => {
-                    let stats = cli.optionStats(urls);
-                    stats.Broken = cli.optionStatsValidate(val).Broken;
-                    console.log(`Total: ${stats.Total}`);
-                    console.log(`Unique: ${stats.Unique}`);
-                    console.log(`Broken: ${stats.Broken}`);
-                })
-            })
-        } else if (options.stats === true) {
-            api.mdLinks(path, options).then((resp) => {
-                let urls = resp;
-                const stats = cli.optionStats(urls);
-                console.log(`Total: ${stats.Total}`)
-                console.log(`Unique: ${stats.Unique}`)
-            })
-        }else {
-            api.mdLinks(path, options).then((resp) => {
-                for (const url of resp) {
-                    if ( options.validate === false ) {
-                        console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text}`)
-                        console.log('------------------------------------------------------------------')
-                    }else {
-                        if(url.status < 299){
-                            console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status} ${'|'.yellow}   U+08A7 ${url.ok.green}`)
-                            console.log('------------------------------------------------------------------')
+            let stats = cli.optionStats(resp);
+            stats.Broken = cli.optionStatsValidate(resp).Broken;
+            console.log(`${ 'Total:'.bold } ${stats.Total}`);
+            console.log(`${ 'Unique:'.bold } ${stats.Unique}`);
+            console.log(`${ 'Broken:'.bold } ${stats.Broken}`);
+            // console.log('otra cosa')
+        } else if (options.stats === true && options.validate === false) {
+            let urls = resp;
+            const stats = cli.optionStats(urls);
+            console.log(`${ 'Total:'.bold } ${stats.Total}`)
+            console.log(`${ 'Unique:'.bold } ${stats.Unique}`)
 
-                        } else {
-                            console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status.red} ${'|'.yellow}  ${url.ok.red}`)
-                            console.log('------------------------------------------------------------------')
-                        }
-                    }
+        } else if ( options.validate === true ) {
+            for (const url of resp) {
+                if(url.status < 299) {
+                    console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status} ${'|'.yellow} ✅ ${url.ok.green}`)
+                    console.log('------------------------------------------------------------------')
+                } else {
+                    console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text} ${'|'.yellow} ${url.status} ${'|'.yellow} ❌ ${url.ok.red}`)
+                    console.log('------------------------------------------------------------------')
                 }
-            });
+            }
+        } else {
+            for (const url of resp) {
+                console.table(`${url.file} ${'|'.yellow} ${url.href} ${'|'.yellow} ${url.text}`)
+                console.log('------------------------------------------------------------------')
+            }
         }
-
     })
+    .catch((error)=> {
+        console.error(error)
+    })
+})
 
 program.parse(process.argv);
 // api.mdLinks(pathSent, options).then((resp) => {
